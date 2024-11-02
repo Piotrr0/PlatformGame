92c8fa4b1ce9e710f2e958c775ea177c4026a2c1
Piotrr0 <piotrr.nawalany@gmail.com>
2024-11-02 11:45:53 +0100

    Improve health reward script

--- Assets/Prefab/Scripts/ObjectInterpolator.cs
+++ Assets/Prefab/Scripts/ObjectInterpolator.cs
@@ -5,19 +5,36 @@ namespace prefab.interp
 {
     public class ObjectInterpolator : MonoBehaviour
     {
-        public IEnumerator InterpolateObject(Vector2 startPosition, Vector2 endPosition, float duration ,GameObject interpedObejct)
+        [SerializeField] private Vector2 startPosition;
+        [SerializeField] private Vector2 endPosition;
+        [SerializeField] private float duration;
+        [SerializeField] private GameObject objectToInterpolate;
+
+        public void StartInterpolate()
+        {
+            if (objectToInterpolate != null)
+            {
+                Vector2 globalStartPosition = transform.TransformPoint(startPosition);
+                Vector2 globalEndPosition = transform.TransformPoint(endPosition);
+
+                GameObject spawnedObject = Instantiate(objectToInterpolate, globalStartPosition, Quaternion.identity);
+                StartCoroutine(InterpolateObject(globalStartPosition, globalEndPosition, duration, spawnedObject));
+            }
+        }
+
+        private IEnumerator InterpolateObject(Vector2 globalStartPosition, Vector2 globalEndPosition, float duration, GameObject objectToInterpolate)
         {
             float elapsedTime = 0f;
 
             while (elapsedTime < duration)
             {
-                interpedObejct.transform.position = Vector2.Lerp(startPosition, endPosition, elapsedTime / duration);
+                objectToInterpolate.transform.position = Vector2.Lerp(globalStartPosition, globalEndPosition, elapsedTime / duration);
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
 
-            interpedObejct.transform.position = endPosition;
-            Destroy(interpedObejct, duration);
+            objectToInterpolate.transform.position = globalEndPosition;
+            Destroy(objectToInterpolate, duration);
         }
     }
 }
--- Assets/Rewards/HealthReward.cs
+++ Assets/Rewards/HealthReward.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using health;
 using prefab.interp;
+using UnityEngine.Events;
 
 namespace reward.health
 {
@@ -8,36 +9,16 @@ namespace reward.health
     public class HealthReward : MonoBehaviour, IReward
     {
         [SerializeField] private Health health;
-        [SerializeField] private GameObject maxHealthIcon;
         [SerializeField] private float bonusHealth;
-
-        private ObjectInterpolator interpolator;
-        private Vector2 interpHeight = new Vector2(0f, 1f);
-
-        private void Awake()
-        {
-            interpolator = GetComponent<ObjectInterpolator>();
-        }
+        [SerializeField] private UnityEvent onHealthRewardCollect;
 
         public void CollectReward()
         {
-            if (health != null && maxHealthIcon != null)
+            if (health != null)
             {
                 health.MaxHealth += bonusHealth;
                 health.Heal();
-                InterpIcon();
-            }
-        }
-
-        private void InterpIcon()
-        {
-            if(interpolator != null)
-            {
-                Vector2 startPosition = transform.position;
-                Vector2 endPosition = startPosition + interpHeight;
-
-                GameObject icon = Instantiate(maxHealthIcon, startPosition, Quaternion.identity);
-                StartCoroutine(interpolator.InterpolateObject(startPosition, endPosition, 0.5f, icon));
+                onHealthRewardCollect?.Invoke();
             }
         }
     }
